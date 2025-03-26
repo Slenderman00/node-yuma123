da151c7780d47843888ed74d375be68dfdf914b6
Joar Heimonen <contact@joar.me>
2025-03-26 14:27:25 +0100

    feat: implemented new example

---
+++ examples/transaction.js
@@ -0,0 +1,15 @@
+const binding = require('../build/Release/yang');
+
+const server = "127.0.0.1";
+const port = 830;
+const username = "user";
+const password = "pass";
+
+const connection = binding.yangrpc.connect(server, port, username, password, null, null, null);
+console.log("Connection:", connection ? "successful" : "failed");
+
+const [cliStatus, rpcData] = binding.yangrpc.parse_cli(connection, "xget /");
+console.log("Command parsed:", cliStatus === 0 ? "yes" : "no");
+
+const [rpcStatus, reply] = binding.yangrpc.rpc(connection, rpcData);
+console.log("Command executed:", rpcStatus === 0 ? "successfully" : "failed");
