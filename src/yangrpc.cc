449c41187465827d52664b34f1cd361d7cad209e
Joar Heimonen <contact@joar.me>
2025-03-27 18:21:45 +0100

    fix: renamed to rpc

--- src/yangrpc.cc
+++ src/yangrpc.cc
@@ -101,7 +101,7 @@ namespace yangrpc {
         args.GetReturnValue().Set(External::New(isolate, yangrpc_cb_ptr));
     }
 
-    void Exec(const FunctionCallbackInfo<Value>& args) {
+    void Rpc(const FunctionCallbackInfo<Value>& args) {
         Isolate* isolate = args.GetIsolate();
 
         if (args.Length() < 2) {
@@ -113,11 +113,7 @@ namespace yangrpc {
         void* rpc_val = External::Cast(*args[1])->Value();
 
         val_value_t* reply_val;
-        status_t res = yangrpc_exec(
-            (yangrpc_cb_ptr_t)yangrpc_cb_ptr, 
-            (val_value_t*)rpc_val, 
-            &reply_val
-        );
+        status_t res = yangrpc_exec((yangrpc_cb_ptr_t)yangrpc_cb_ptr, (val_value_t*)rpc_val, &reply_val);
 
         Local<Array> result = Array::New(isolate, 2);
         result->Set(isolate->GetCurrentContext(), 0, Number::New(isolate, res));
@@ -181,7 +177,7 @@ namespace yangrpc {
 
     void InitYangRpc(Local<Object> exports) {
         NODE_SET_METHOD(exports, "connect", Connect);
-        NODE_SET_METHOD(exports, "rpc", Exec);
+        NODE_SET_METHOD(exports, "rpc", Rpc);
         NODE_SET_METHOD(exports, "parse_cli", ParseCli);
         NODE_SET_METHOD(exports, "close", Close);
     }
