abb686dda2433c2b5ac9daa0d95c0aa0afb92dd7
Joar Heimonen <contact@joar.me>
2025-03-31 19:18:43 +0200

    feat: yangcli command

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 const yuma123 = require('./build/Release/yang');
-module.exports = yuma123;
+
+const yangcli = (connection, cmd) =>  {
+    const [cliStatus, rpcData] = yuma123.yangrpc.parse_cli(connection, cmd);
+
+    if(!cliStatus) {
+        //Implement error handling
+    }
+
+    const [rpcStatus, reply] = yuma123.yangrpc.rpc(connection, rpcData);
+
+    if(!rpcStatus) {
+        //Implement error handling
+    }
+
+    yuma123.yuma.init();
+    const [res, output] = yuma123.yuma.val_make_serialized_string(reply, 5);
+
+    return output;
+}
+
+module.exports = {...yuma123, yangcli };
