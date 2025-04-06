2879ff5a18e9f737d5961d77c07a21e6a56e58db
Joar Heimonen <contact@joar.me>
2025-04-06 16:27:34 +0200

    feat: error handling

--- examples/yangcli.js
+++ examples/yangcli.js
@@ -1,12 +1,12 @@
-import { yuma123, yangcli } from "../index.js";
+import { NCX_DISPLAY_MODE_XML_NONS, yangcli, safeConnect} from "../index.js";
 
 const server = "127.0.0.1";
 const port = 830;
 const username = "user";
 const password = "pass";
 
-const connection = yuma123.yangrpc.connect(server, port, username, password, null, null, null);
+const connection = safeConnect(server, port, username, password, null, null, null);
 
-let res = yangcli(connection, "xget /", yuma123.NCX_DISPLAY_MODE_XML_NONS);
+let res = yangcli(connection, "xget /", NCX_DISPLAY_MODE_XML_NONS);
 
 console.log(res);
