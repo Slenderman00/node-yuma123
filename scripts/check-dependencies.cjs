1f54d9de75777f45199959a41e8ab420f37db4fb
Joar Heimonen <contact@joar.me>
2025-04-09 16:05:25 +0200

    fix: added check for yangrpc

--- scripts/check-dependencies.cjs
+++ scripts/check-dependencies.cjs
@@ -4,3 +4,8 @@ if(!fs.existsSync("/usr/include/yuma")) {
     console.error("Yuma123 not found at path: /usr/include/yuma")
     process.exit(1);
 }
+
+if(!fs.existsSync("/usr/include/yuma/yangrpc")) {
+    console.error("Yangrpc not found at path: /usr/include/yuma/yangrpc")
+    process.exit(1);
+}
