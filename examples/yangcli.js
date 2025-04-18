809a1ec8f98916cd47f7043fcb34a1883358f440
Joar Heimonen <contact@joar.me>
2025-04-18 16:58:40 +0200

    feat: added yangrpc close to the example

--- examples/yangcli.js
+++ examples/yangcli.js
@@ -1,4 +1,4 @@
-import { NCX_DISPLAY_MODE_XML_NONS, yangcli, safeConnect} from "../index.js";
+import { NCX_DISPLAY_MODE_XML_NONS, yangcli, safeConnect, yuma123} from "../index.js";
 
 const server = "127.0.0.1";
 const port = 830;
@@ -6,7 +6,6 @@ const username = "user";
 const password = "pass";
 
 const connection = safeConnect(server, port, username, password, null, null, null);
-
 let res = yangcli(connection, "xget /", NCX_DISPLAY_MODE_XML_NONS);
 
-console.log(res);
+yuma123.yanrpc.close(connection)
